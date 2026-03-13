fd6cfc544e23887c40ee56f8fbe9feba49327efd
Doni Wahyudi <doni.wahyudi@btpnsyariah.com>
2026-03-13 21:45:56 +0700

    update maps

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -188,7 +188,7 @@ export default function Contact() {
               <div className="contact-map">
                 <iframe
                   title="Lokasi CV Putra Karya Abadi"
-                  src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d15865.5!2d107.15!3d-6.32!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e6991!2sCikarang%20Utara%2C%20Bekasi!5e0!3m2!1sen!2sid!4v1"
+                  src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3965.2343950000004!2d107.1420174!3d-6.2820568!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x0!2zNsKwMTYnNTUuNCJTIDEwN8KwMDgnNDAuNSJF!5e0!3m2!1sen!2sid!4v1710330000000!5m2!1sen!2sid"
                   width="100%"
                   height="220"
                   style={{ border: 0, borderRadius: 'var(--radius-lg)' }}
